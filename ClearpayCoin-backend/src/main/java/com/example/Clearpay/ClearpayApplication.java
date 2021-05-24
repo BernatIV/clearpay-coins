9694cc56638fa542220ec341683b17eb0a39fe8e
BernatIV <marc.mane.duatis@gmail.com>
2021-05-24 15:27:58 +0200

    Init repo

---
+++ ClearpayCoin-backend/src/main/java/com/example/Clearpay/ClearpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clearpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearpayApplication.class, args);
+	}
+
+}
---
+++ ClearpayCoin-backend/src/test/java/com/example/Clearpay/ClearpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clearpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
